3767980f4eb339e2a70518d836f93936452206ff
Eduardo Motta de Oliveira <edumottaoliveira@gmail.com>
2019-08-07 21:10:22 -0300

    alguma mudança

--- TestingDangerApp/ViewController.swift
+++ TestingDangerApp/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  TestingDangerApp
-//
-//  Created by PicPay Eduardo on 7/23/19.
-//  Copyright © 2019 PicPay Eduardo. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -18,7 +10,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
