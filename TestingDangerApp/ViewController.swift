1954ce2c1a721bee3c82d7f1ed2048f9858a4acd
Eduardo Motta de Oliveira <edumottaoliveira@gmail.com>
2019-08-09 19:46:07 -0300

    adiciona swift lint

--- TestingDangerApp/ViewController.swift
+++ TestingDangerApp/ViewController.swift
@@ -1,19 +1,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-    lazy var label: UILabel = {
+    lazy var label:UILabel = {
         let label = UILabel()
-        label.text = "bla bla bla"
+        label.text = "Olá mundo!"
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        view.backgroundColor = .yellow
+        addComponents()
+        layoutComponents()
+        
+    }
+    
+    func addComponents() {
+        view.addSubview(label)
+    }
+    
+    func layoutComponents() {
+        
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
 }
-
