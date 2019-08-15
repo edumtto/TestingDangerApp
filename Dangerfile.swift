016c5a9ab3f21ef85e4273ba3e2934bfcbebfa20
Unknown <edumottaoliveira@gmail.com>
2019-08-14 23:18:30 -0300

    Edita novamente o dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,19 +1,13 @@
 import Danger
+
 let danger = Danger()
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
-// Pull out the edited files and find ones that come from a sub-folder
-// where our app lives
-let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-let editedAppFiles = editedFiles.filter { $0.contains("/App") }
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
 
-// Let people bail from the CHANGELOG check
-let hasSkipChangelog = danger.github.pullRequest.body?.contains("#no_changelog") ?? false
-let hasSkipChangelogLabel = danger.github.issue.labels.first { $0.name == "Skip Changelog" }
-let skipCheck = hasSkipChangelog || hasSkipChangelogLabel
-
-// Request for a CHANGELOG entry with each app change
-if editedAppFiles.count > 0 && !skipCheck {
-  fail("Please add a CHANGELOG entry for these changes. If you would like to skip this check, add `#no_changelog` to the PR body and re-run CI.")
+if !changelogChanged && sourceChanges != nil {
+  warn("No CHANGELOG entry added.")
 }
 
 // Instead of making a markdown table in the main message
