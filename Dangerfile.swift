d88cc96a684935a7a9b2c2b7233dfcfbf8b4b71e
Unknown <edumottaoliveira@gmail.com>
2019-08-14 22:29:06 -0300

    Edita dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -11,8 +11,9 @@ if !changelogChanged && sourceChanges != nil {
 }
 
 // You can use these functions to send feedback:
-message("Highlight something in the table")
+/*message("Highlight something in the table")
 warn("Something pretty bad, but not important enough to fail the build")
 fail("Something that must be changed")
 
 markdown("Free-form markdown that goes under the table, so you can do whatever.")
+*/
