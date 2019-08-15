8cf278f37624a59228cd35e462628281ce36c3fc
Unknown <edumottaoliveira@gmail.com>
2019-08-14 22:54:36 -0300

    Muda changefile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,14 +1,25 @@
 import Danger
-
 let danger = Danger()
-let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
-let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
-let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+// Pull out the edited files and find ones that come from a sub-folder
+// where our app lives
+let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
+let editedAppFiles = editedFiles.filter { $0.contains("/App") }
 
-if !changelogChanged && sourceChanges != nil {
-  warn("No CHANGELOG entry added.")
+// Let people bail from the CHANGELOG check
+let hasSkipChangelog = danger.github.pullRequest.body.contains("#no_changelog")
+let hasSkipChangelogLabel = danger.github.issue.labels.first { $0.name == "Skip Changelog" }
+let skipCheck = hasSkipChangelog || hasSkipChangelogLabel
+
+// Request for a CHANGELOG entry with each app change
+if editedAppFiles.count > 0 && !skipCheck {
+  fail("Please add a CHANGELOG entry for these changes. If you would like to skip this check, add `#no_changelog` to the PR body and re-run CI.")
 }
+
+// Instead of making a markdown table in the main message
+// sprinkle those comments inline, this can be a bit noisy
+// but it definitely feels magical.
+SwiftLint.lint(inline: true)
 
 // You can use these functions to send feedback:
 /*message("Highlight something in the table")
