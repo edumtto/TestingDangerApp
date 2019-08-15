374eeebfa9de5783d7d1019ec352e717330fb7bd
Unknown <edumottaoliveira@gmail.com>
2019-08-14 23:04:32 -0300

    Edita dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -7,7 +7,7 @@ let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 let editedAppFiles = editedFiles.filter { $0.contains("/App") }
 
 // Let people bail from the CHANGELOG check
-let hasSkipChangelog = danger.github.pullRequest.body.contains("#no_changelog")
+let hasSkipChangelog = danger.github.pullRequest.body?.contains("#no_changelog") ?? false
 let hasSkipChangelogLabel = danger.github.issue.labels.first { $0.name == "Skip Changelog" }
 let skipCheck = hasSkipChangelog || hasSkipChangelogLabel
 
